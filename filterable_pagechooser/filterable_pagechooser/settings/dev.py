d44aa5b98b55aa65dbbf9d0a30960528fe0062c6
Aya Joumaa <aya.joumaait@gmail.com>
2025-01-11 22:17:39 +0300

    project configurations

---
+++ filterable_pagechooser/filterable_pagechooser/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)rz$+ud$oi1$&w(@$^dtr_@_p5*fk_n)h!_ih6)37j$h=ng4hb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
