61451ea144f0f8bede31c36f06c0889ed89014ec
Aya Joumaa <aya.joumaait@gmail.com>
2025-01-11 23:32:05 +0300

    adding the filterable page chooser block [2]

--- filterable_pagechooser/filterable_pagechooser/urls.py
+++ filterable_pagechooser/filterable_pagechooser/urls.py
@@ -7,6 +7,9 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+from .views import (
+    FilterPageBrowseView,
+)
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -14,7 +17,6 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 ]
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
@@ -28,6 +30,16 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
+    path(
+        "filter-choose-page/",
+        FilterPageBrowseView.as_view(),
+        name="filter_wagtail_admin_choose_page"
+    ),
+    path(
+        "filter-choose-page/<int:parent_page_id>/",
+        FilterPageBrowseView.as_view(),
+        name="filter_wagtail_admin_choose_page_child",
+    ),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
---
+++ filterable_pagechooser/filterable_pagechooser/views.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.core.exceptions import FieldError
+from django.utils.functional import cached_property
+from wagtail.admin.views.chooser import BrowseView
+from wagtail.models import Page
+
+
+class UserPermsForm(forms.Form):
+    user_perms = forms.CharField(required=False)
+
+    class Meta:
+        fields = "__all__"
+
+
+class FilterPageBrowseView(BrowseView):
+    def get(self, request, parent_page_id=None):
+
+        self.user_perm = None
+        perms_form = UserPermsForm(request.GET)
+
+        if perms_form.is_valid():
+            self.user_perm = perms_form.cleaned_data.get(
+                "user_perms", None
+            )
+        return super().get(request, parent_page_id=None)
+
+    @cached_property
+    def prepare_filter(self):
+        if not self.user_perm:
+            return None
+
+        type_filter = {}
+        filter_info = self.user_perm.split(",")[0]
+
+        if filter_info.split(":")[0] == "filters_info":
+            filter_info = filter_info.split(":")[1]
+
+            for info in filter_info.split(";"):
+                type_filter.update({
+                    info.split("=")[0]: info.split("=")[1],
+                })
+            return type_filter
+
+        return None
+
+    def get_object_list(self):
+        # Get children of parent page (without streamFields)
+        pages = self.parent_page.get_children().defer_streamfields().specific()
+
+        type_filter = self.prepare_filter
+        model_name = pages[0].__class__
+
+        if type_filter:
+            try:
+                pages = model_name.objects.filter(**type_filter)
+            except FieldError:
+                pages = Page.objects.none()
+
+        if self.i18n_enabled:
+            pages = pages.select_related("locale")
+
+        return pages
